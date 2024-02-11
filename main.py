7ded5dde7dff8255ef7d7000b110b207472f21ea
Robson Cruz <ghost0092@gmail.com>
2024-02-11 13:09:51 -0300

    refactor(main.py): file reading to as own function

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
+def read_file_contents(filepath: str) -> str:
+    with open(filepath) as f:
+        return f.read()
+
 def count_words(string: str) -> int:
     return len(string.split())
 
 def main():
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read()
-        print(file_contents)
-        print(f"Word count: {count_words(file_contents)}")
+    file_contents = read_file_contents("books/frankenstein.txt")
+    print(file_contents)
+    print(f"Word count: {count_words(file_contents)}")
 
 if __name__ == "__main__":
     main()
