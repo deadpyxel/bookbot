958c8439db39fdd076fca439d30cdd1369291e03
Robson Cruz <ghost0092@gmail.com>
2024-02-11 13:07:36 -0300

    feat(main.py): add word_count function

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
+def count_words(string: str) -> int:
+    return len(string.split())
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
         print(file_contents)
+        print(f"Word count: {count_words(file_contents)}")
 
 if __name__ == "__main__":
     main()
